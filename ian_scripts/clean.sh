8597843c4f00667a45415a0d67c9f2483c79779f
Ian Rakhmatullin <Ian_Rakhmatullin@epam.com>
2021-12-29 01:31:24 +0300

    Fixed metrics

---
+++ ian_scripts/clean.sh
@@ -0,0 +1,5 @@
+helm uninstall controlcenter -n operator
+helm uninstall connectors -n operator
+helm uninstall kafka -n operator
+helm uninstall zookeeper -n operator
+helm uninstall operator -n operator
